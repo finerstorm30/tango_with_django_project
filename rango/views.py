28737aebfa93c5252ed856e7f23afd631957f60b
Ben Clydesdale <2768789C@student.gla.ac.uk>
2025-02-12 15:52:46 +0000

    chapter 9, logout created and base.html links tidied

--- rango/views.py
+++ rango/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import redirect
 from django.urls import reverse
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 from rango.models import Category
 from rango.models import Page
@@ -145,3 +146,13 @@ def user_login(request):
         
     else:
         return render(request, 'rango/login.html')
+    
+@login_required
+def restricted(request):
+    return HttpResponse("Since you're logged in, you can see this text!")
+
+@login_required
+def user_logout(request):
+    logout(request)
+
+    return redirect(reverse('rango:index'))
